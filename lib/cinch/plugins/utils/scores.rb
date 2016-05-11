7b73c27a552a86944ddb040b288ab3b81add2197
Michael Bazzinotti <mbazzinotti@gmail.com>
2016-05-11 13:01:11 -0400

    [scores] do not truncate score listing

--- lib/cinch/plugins/utils/scores.rb
+++ lib/cinch/plugins/utils/scores.rb
@@ -6,10 +6,10 @@ module Cinch::Plugins::Utils
     def truncate_n(m, n)
       n = n.to_i
       n = 5 if n == 0 # TODO config[:scores] instead of hardcoded default
-      if n > 10
-        n = 10
-        m.reply "I will only print the Top 10 to avoid spam :3\n"
-      end
+      # if n > 10
+      #   n = 10
+      #   m.reply "I will only print the Top 10 to avoid spam :3\n"
+      # end
       n
     end
   end
