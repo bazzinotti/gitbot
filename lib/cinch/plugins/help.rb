701a8feb321c7884e775e023a3bb1483ead679ed
Michael J. Bazzinotti <mbazzinotti@gmail.com>
2016-05-14 04:08:49 -0400

    [plugin/help] update copyright

--- lib/cinch/plugins/help.rb
+++ lib/cinch/plugins/help.rb
@@ -87,6 +87,7 @@
 # == License
 # A help plugin for Cinch.
 # Copyright © 2012 Marvin Gülker
+# Copyright © 2016 Michael Bazzinotti
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
